fa31fd46e709ac4adba80bd516b1290dd56f43e9
Ilya Zhuravlev <whatever@xyz.is>
2025-08-16 21:39:31 -0600

    update version to Cython==0.29.25

--- setup.py
+++ setup.py
@@ -126,6 +126,6 @@ setup(
         'Programming Language :: Python :: 3.9',
     ],
     ext_modules = modules,
-    setup_requires = ['Cython==3.0.0a9'],
+    setup_requires = ['Cython==0.29.25'],
     install_requires = ['setuptools>=19.0'],
 )
